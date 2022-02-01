38a59c0bc39badef9a9d53a70bad089cafc7883f
Emilio Dalla Torre <info@emiliodallatorre.it>
2022-02-01 22:09:55 +0100

    Creato il tema figlio.
    Took 7 minutes

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function childtheme_parent_styles()
+{
+    // enqueue style
+    wp_enqueue_style('parent', get_template_directory_uri() . '/style.css');
+}
+
+add_action('wp_enqueue_scripts', 'childtheme_parent_styles');
