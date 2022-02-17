83f196b0fe29b0d2a6e165cab3d622ddd5d8ccb0
Emilio Dalla Torre <info@emiliodallatorre.it>
2022-02-17 17:47:46 +0100

    Preparato il pulsante per la creazione di un ordine, dei colori giusti.
    Took 48 minutes

--- functions.php
+++ functions.php
@@ -1,9 +1,7 @@
 <?php
 
-function childtheme_parent_styles()
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles', 11);
+function my_theme_enqueue_styles()
 {
-    // enqueue style
-    wp_enqueue_style('parent', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_uri());
 }
-
-add_action('wp_enqueue_scripts', 'childtheme_parent_styles');
